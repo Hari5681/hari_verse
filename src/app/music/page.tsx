b147a54835620d5d273b15dd737a2658fabe6778
Hari Krishna <rowdylucky888@gmail.com>
2025-09-06 17:11:23 +0000

    recelly played songs show upto 5

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -148,7 +148,7 @@ export default function MusicPage() {
                     <section>
                         <h2 className="text-2xl font-bold mb-4">Recently Played</h2>
                          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
-                            {recentlyPlayed.map((song) => (
+                            {recentlyPlayed.slice(0, 5).map((song) => (
                                 <RecentlyPlayedSongCard key={`recent-${song.key}`} song={song} onPlay={() => handlePlaySong(song, recentlyPlayed)} />
                             ))}
                         </div>
