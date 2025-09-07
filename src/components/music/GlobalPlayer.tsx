52c1f469fbd7c1e703484425721724d4be801f1c
Hari Krishna <rowdylucky888@gmail.com>
2025-09-07 05:14:17 +0000

    when closed dont show it  (_for element <Player>_)

--- src/components/music/GlobalPlayer.tsx
+++ src/components/music/GlobalPlayer.tsx
@@ -7,9 +7,13 @@ import { Player } from './Player';
 export function GlobalPlayer({ forceShow = false }: { forceShow?: boolean }) {
     const { currentSong } = useMusicPlayer();
 
+    // The player should only be visible if there's a song loaded,
+    // or if we are forcing it to show (on music pages).
     if (!currentSong && !forceShow) {
         return null;
     }
     
+    // If we forceShow but have no song, the Player component will render a placeholder.
+    // Otherwise, it will render the full or mini player.
     return <Player />;
 }
